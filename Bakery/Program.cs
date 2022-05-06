386692d053b8d8cd2efd0666db2c13c299ffba36
Jake Edgar <jakeedgar1012@gmail.com>
2022-05-06 15:55:50 -0700

    update console interface to function correctly

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,28 +6,34 @@ class Program
   static void Main()
   {
     Console.WriteLine("Welcome to Pierre's Bakery!");
-    Console.WriteLine("Would you like bread or pastry today?");
-    string userInput = Console.ReadLine().ToLower();
-    if (userInput == "bread")
+    while (true)
+    {
+    Console.WriteLine("Would you like to place an order today Y/N?");
+    string userInput = Console.ReadLine().ToUpper();
+    if (userInput == "Y")
     {
       Console.WriteLine("How many loaves of bread today?");
       int loafOrder = int.Parse(Console.ReadLine());
       Bread newBread = new Bread(loafOrder);
-      return newBread;
-    }
-    else if (userInput == "pastry")
-    {
       Console.WriteLine("How many pastries today?");
       int pastryOrder = int.Parse(Console.ReadLine());
       Pastry newPastry = new Pastry(pastryOrder);
-      return newPastry;
+      int breadCost = newBread.GetBreadPrice();
+      int pastryCost = newPastry.GetPastryPrice();
+      int totalPrice = breadCost + pastryCost;
+      Console.WriteLine(totalPrice);
+      Console.WriteLine("Thank you for your order today.");
+      Console.WriteLine("You ordered " + newPastry.PastryAmount + " pastries and " + newBread.BreadAmount + " loaves of bread it will cost you " + totalPrice + " dollars");
+    }
+    else if (userInput == "N")
+    {
+      Console.WriteLine("Have a nice day.");
+      break;
     }
     else 
     {
       Console.WriteLine("Please select either bread or pastry.");
     }
-    int totalPrice = newBread.BreadPrice + newPastry.PastryPrice;
-    Console.WriteLine("Thank you for your order today.");
-    Console.WriteLine("You ordered " + newPastry.PastryAmount + " of pastries and " + newBread.BreadAmount + " loaves of bread it will cost you " + totalPrice + " dollars");
+    }
   }
 }
